477855da107b1a4380555d820aa4bbc87301a96a
Si Hyeong Lee <super2451894@gmail.com>
2019-11-14 21:03:16 +0900

    Update Menu.jsx
    - Add flexGrow CSS

--- src/app/components/Menu.jsx
+++ src/app/components/Menu.jsx
@@ -31,6 +31,9 @@ const useStyles = makeStyles(theme => ({
   root: {
     display: "flex"
   },
+  grow: {
+    flexGrow: 1
+  },
   appBar: {
     transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
@@ -317,38 +320,3 @@ export default function User() {
     </div>
   );
 }
-
-// import React, { Component } from "react";
-// import { BrowserRouter as Router, Link } from "react-router-dom";
-
-// class User extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-
-//   render() {
-//     return (
-//       <div className="navigation">
-//         <div className="menu">
-//           <ul>
-//             <li>
-//               <Link to="/">HOME</Link>
-//             </li>
-//             <li>
-//               <Link to="/users">Users</Link>
-//             </li>
-//             <li>
-//               <Link to="/auth/login">Login</Link>
-//             </li>
-//             <li>
-//               <a href="/auth/res">RES</a>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default User;
