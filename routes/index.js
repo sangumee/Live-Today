c21771dc55694789e5f3a0837edb3a33b3ae250c
Si Hyeong Lee <super2451894@gmail.com>
2021-08-25 17:11:36 +0900

    Update index.js
    - Index Main Router
    - Install got package to Request data
    - Create /test sample router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+/* Installed Package */
+const got = require('got');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('main', { title: 'Live Today' });
+});
+
+router.get('/test', async function(req, res, next) {
+  let testData = await got.post('https://httpbin.org/anything', {
+    json: {
+      hello: 'world'
+    }
+  }).json();
+  console.log(testData);
+  res.send('1');
 });
 
 module.exports = router;
